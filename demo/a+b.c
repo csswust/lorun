f610cc296427e52184a3a692e858febcd2d2eead
yangshunfeng <972536780@qq.com>
2018-03-29 18:32:44 +0800

    判题模块

--- demo/a+b.c
+++ demo/a+b.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main() {
     int a, b;
-    while(scanf("%d %d", &a, &b) != EOF)
-    {
-        printf("%d\n", a + b);
-    }
+    
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    // open("asdf", O_CREAT);
+    /*scanf("%d", &a);*/
+    
     return 0;
 }
